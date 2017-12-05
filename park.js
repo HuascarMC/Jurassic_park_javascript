9a14686195805b38596d7722c1353ba47d9a85a1
Huascar <huascarmc@hotmail.com>
2017-12-05 18:12:51 +0000

    fixed bug in remove method, test passing

--- park.js
+++ park.js
@@ -9,7 +9,7 @@ Park.prototype.add = function(dinosaur) {
 Park.prototype.remove = function(type) {
  for (var dinosaur of this.enclosure) {
   if (dinosaur.type == type)
-  var index = this.enclosure.ofIndex(dinosaur);
+  var index = this.enclosure.indexOf(dinosaur);
   this.enclosure.splice(index, 1);
  }
 }
