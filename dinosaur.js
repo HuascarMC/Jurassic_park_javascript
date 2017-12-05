16b451f98593f96c9b4f3de8d9c69f4032f458d0
Huascar <huascarmc@hotmail.com>
2017-12-05 20:28:09 +0000

    dinosaur properties are accessed by method

--- dinosaur.js
+++ dinosaur.js
@@ -3,4 +3,12 @@ const Dinosaur = function(type, offsprings) {
  this.offsprings = offsprings;
 }
 
+Dinosaur.prototype.getType = function() {
+ return this.type;
+}
+
+Dinosaur.prototype.getOffsprings = function() {
+ return this.offsprings;
+}
+
 module.exports = Dinosaur;
--- specs/dinosaur_spec.js
+++ specs/dinosaur_spec.js
@@ -5,11 +5,11 @@ describe('Dinosaur', function() {
 
  it('should have a type', function() {
   const dinosaur = new Dinosaur('Tyrannosaurus', 2);
-  assert.strictEqual(dinosaur.type, 'Tyrannosaurus');
+  assert.strictEqual(dinosaur.getType(), 'Tyrannosaurus');
  })
 
  it('should have offsprings', function() {
   const dinosaur = new Dinosaur('Tyrannosaurus', 2);
-  assert.strictEqual(dinosaur.offsprings, 2);
+  assert.strictEqual(dinosaur.getOffsprings(), 2);
  })
 })
