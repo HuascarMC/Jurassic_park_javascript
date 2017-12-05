c3a8a2932f877b1ab567d1a8b8c87c10c9f4c1a0
Huascar <huascarmc@hotmail.com>
2017-12-05 18:10:48 +0000

    added remove method by type to park

--- park.js
+++ park.js
@@ -5,4 +5,13 @@ const Park = function() {
 Park.prototype.add = function(dinosaur) {
  this.enclosure.push(dinosaur);
 }
+
+Park.prototype.remove = function(type) {
+ for (var dinosaur of this.enclosure) {
+  if (dinosaur.type == type)
+  var index = this.enclosure.ofIndex(dinosaur);
+  this.enclosure.splice(index, 1);
+ }
+}
+
 module.exports = Park;
